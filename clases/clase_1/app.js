bbe377e96c81b46d9d61c97b670596844f342abd
Martin Rivas Carranza <martinrivascarranza@gmail.com>
2024-04-13 00:12:57 +0100

    first commit

---
+++ clases/clase_1/app.js
@@ -0,0 +1,45 @@
+/*
+
+comentario en linea vacia
+
+*/
+
+//comentario en linea
+
+//variables
+
+// no usar la variable var
+
+//let 
+
+// let name = ' Martin';
+// let age = 32; //se recomienda iniciar una variable con un valor
+// let nationality = 'portuguese';
+
+// //const
+// const birthDate = '10/08/1991';
+
+
+// let valorUno = 45
+// let valorDos = 50
+// let valorTres = 27
+// let valorCuatro = 17
+
+
+// let resultado = valorUno + valorDos
+
+// console.log(resultado)
+// console.log(name)
+
+
+// alert(resultado + name);
+
+let primerNumero = prompt('ingrese un número')
+primerNumero = parseInt(primerNumero)
+let segundoNumero = prompt('ingrese otro número')
+segundoNumero = parseInt(segundoNumero)
+alert(primerNumero + segundoNumero)
+
+
+
+
---
+++ desafios/desafio_1/app.js
@@ -0,0 +1,13 @@
+
+let variableNumero = 37
+
+let nombreApellido = 'Martin Leonardo'
+
+let variableVerdad = true
+
+let variableMentira = false
+
+
+console.log(variableNumero);
+
+console.log(nombreApellido)
